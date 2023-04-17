ead38dffdfcb2fd9a2e1f0961d02e116e322e873
Soeren <nikorpoulsen@gmail.com>
2023-04-17 21:45:35 +0200

    change the name of the top module take 3

--- src/tb.v
+++ src/tb.v
@@ -26,7 +26,7 @@ module tb (
     assign segments = outputs[6:0];
 
     // instantiate the DUT
-    seven_segment_seconds seven_segment_seconds(
+    nikor_clock nikor_clock(
         `ifdef GL_TEST
             .vccd1( 1'b1),
             .vssd1( 1'b0),
